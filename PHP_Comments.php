b7579ee46d60cfd80345d108436423a50c077ff3
Mennouchi Islam Azeddine <azeddine.mennouchi@owasp.org>
2014-10-16 20:24:06 +0200

    Update PHP_Comments.php

--- PHP_Comments.php
+++ PHP_Comments.php
@@ -1,6 +1,6 @@
 <?php
 /*
-##      Copyright (C) 2014 Mennouchi Islam Azeddine <azeddine.mennouchi@owasp.org>
+##      Coded by Mennouchi Islam Azeddine <azeddine.mennouchi@owasp.org>
 ##     This program is free software: you can redistribute it and/or modify
 ##     it under the terms of the GNU Affero General Public License as published
 ##     by the Free Software Foundation, either version 3 of the License, or
